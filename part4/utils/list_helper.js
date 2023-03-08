19a320a4afeb107d2e4a4dd0bf3ac67795e7a92a
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-03-08 13:18:46 +0200

    helper functions and unit tests, step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,16 +2,30 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const totalLikes = (array) => {
+const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes;
   };
 
-  const total = array.reduce(reducer, 0);
+  const total = blogs.reduce(reducer, 0);
 
-  return array.length === 0 ? 0 : total;
+  return blogs.length === 0 ? 0 : total;
 };
+
+const favoriteBlog = (blogs) => {
+  const reducer = (mostFavoriteBlog, blog) => {
+    if (mostFavoriteBlog.likes > blog.likes) {
+      return mostFavoriteBlog;
+    } else {
+      return blog;
+    }
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
