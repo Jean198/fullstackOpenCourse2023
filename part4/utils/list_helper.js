56908e094f3f9a997bc2adb0ed002168f1c4e86c
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-03-08 11:28:22 +0200

    helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  const total = array.reduce(reducer, 0);
+
+  return array.length === 0 ? 0 : total;
+};
 module.exports = {
   dummy,
+  totalLikes,
 };
